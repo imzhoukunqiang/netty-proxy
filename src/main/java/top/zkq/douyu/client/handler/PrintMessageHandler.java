e492851a554178c71874e9fb9a036e3a42c8eda4
zhoukunqiang <zhoukunqiang@0easy.com>
2019-07-23 10:45:15 +0800

    优化一下client

--- src/main/java/top/zkq/douyu/client/handler/PrintMessageHandler.java
+++ src/main/java/top/zkq/douyu/client/handler/PrintMessageHandler.java
@@ -8,6 +8,12 @@ import org.slf4j.LoggerFactory;
 import top.zkq.douyu.client.entity.DyData;
 import top.zkq.douyu.client.entity.DyMessage;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.text.MessageFormat;
+
 /**
  * @author zkq
  * @version 1.0
@@ -18,6 +24,12 @@ public class PrintMessageHandler extends SimpleChannelInboundHandler<DyMessage> 
     private static final Logger LOGGER = LoggerFactory.getLogger(PrintMessageHandler.class);
     public static final String TYPE = "type";
 
+    public Writer writer;
+
+    public PrintMessageHandler() throws IOException {
+        writer = new BufferedWriter(new FileWriter("D:\\data\\弹幕.txt"));
+    }
+
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, DyMessage msg) throws Exception {
         DyData data = msg.getData();
@@ -27,9 +39,22 @@ public class PrintMessageHandler extends SimpleChannelInboundHandler<DyMessage> 
             String level = data.getString("level");
             String txt = data.getString("txt");
             String bnn = data.getString("bnn");
-            LOGGER.info("[{} {}] [lv.{} {}] {}", uid, nn, level, bnn, txt);
+            String format = MessageFormat.format("[{0} {1}] [lv.{2} {3}] {4}", uid, nn, level, bnn, txt);
+            writer.write(format);
+            writer.write('\n');
+            LOGGER.info(format);
         } else {
             //    System.out.println(data.encode());
         }
+    }
+
+    @Override
+    public void channelUnregistered(ChannelHandlerContext ctx) throws Exception {
+        try {
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        super.channelUnregistered(ctx);
     }
 }
